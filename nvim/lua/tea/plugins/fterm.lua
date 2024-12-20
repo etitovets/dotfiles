8e730026c1b8db8ae6df0618172425d5554e86cf
Evgeny Titovets <e.titovets@naveksoft.com>
2024-12-20 23:24:45 +0300

    add fterm plugin

---
+++ nvim/lua/tea/plugins/fterm.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "numToStr/FTerm.nvim",
+    config = function()
+      local fterm = require("FTerm")
+      vim.keymap.set("n", "<leader>tt", function()
+        fterm:toggle()
+      end, { desc = "[T]oggle [T]terminal" })
+      vim.keymap.set("t", "<leader>tt", function()
+        fterm:toggle()
+      end, { desc = "[T]oggle [T]erminal" })
+    end,
+  },
+  {
+    "numToStr/FTerm.nvim",
+    name = "FTerm.LazyGit.nvim",
+    config = function()
+      local fterm = require("FTerm")
+      local lazygit = fterm:new({
+        cmd = "lazygit",
+      })
+
+      vim.keymap.set("n", "<leader>lg", function()
+        lazygit:toggle()
+      end, { desc = "Toggle [L]azy[G]it" })
+      vim.keymap.set("t", "<leader>lg", function()
+        lazygit:toggle()
+      end, { desc = "Toggle [L]azy[G]it" })
+    end,
+  },
+}
